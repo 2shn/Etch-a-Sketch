605515425c62a85ab22cd83e6261e110bf651c30
Sofia <honhauyen2004@gmail.com>
2025-08-20 17:06:03 +0700

    Feat: Finished random colros mode

---
+++ app.js
@@ -0,0 +1,102 @@
+// IMPORT ELEMENTS ! >:3 🌷
+
+const numSquaresInput = document.querySelector('input[type="text"]');
+const createBtn = document.querySelector('button.create');
+const mode = document.querySelector('ul.modes');
+const leftSide = document.querySelector('div.left');
+const container = document.querySelector('div.container');
+
+//1. Check if input is valid, if there's none, announce
+//2. When grid is created, add the "mouseenter/mouseover" event listener
+//3. When mouse enters, the square changes its color
+//4. When a different mode is clicked, remove the previous event listener
+//4a. Add the new event listener for the new mode
+
+// GLOBAL VARIABLES🌷
+let numSquares = 0;
+const opacity = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
+
+let randomColor = () => {
+    const colorCode = Math.floor(Math.random() * 256);
+    return colorCode;
+}
+
+// GLOBAL FUNCTIONS 🌷
+
+function addAnnouncement(parentElement, message) {
+    const announcement = document.createElement('p')
+    announcement.style.color = 'red';
+    announcement.style.marginBottom = '20px';
+    announcement.textContent = message;
+    parentElement.appendChild(announcement);
+    setTimeout(() => parentElement.removeChild(announcement), 1000)
+}
+
+function checkIfValid() {
+    if (numSquaresInput.value === '') {
+        addAnnouncement(leftSide, 'You must enter a number first !');
+    } else if (isNaN(numSquaresInput.value)) {
+        addAnnouncement(leftSide, 'You must enter a number !!')
+    } else if (Number(numSquaresInput.value) > 100 || Number(numSquaresInput.value) <= 0) {
+        addAnnouncement(leftSide, 'Number must be less than 101 and greater than 0 !');
+    }else {
+        numSquares = Number(numSquaresInput.value);
+        createGrid()
+    }
+}
+
+function createGrid() {
+    const childrenList = container.children
+    
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+
+    if (numSquares < 40) {
+        container.style.maxWidth = '370px';
+    } else if (numSquares < 50) {
+        container.style.maxWidth = '500px';
+    } else {
+        container.style.maxWidth = '65vw';
+    }
+
+    for (let i=0; i<numSquares ; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j=0 ; j<numSquares ; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+    }
+    
+    let squares = document.querySelectorAll('div.square');
+    switchRandomColors(squares)
+}
+
+function switchRandomColors(squares) {
+    for (square of squares) {
+        square.addEventListener('mouseenter', function(e) {
+            e.target.setAttribute('data-hasEventListener', 'true')
+            e.target.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`
+        })
+    }
+}
+
+// EVENTS LISTENERS 🌷
+
+createBtn.addEventListener('click', checkIfValid)
+mode.addEventListener('click', (e) => {
+    if (e.target.id === 'random-colors') {
+
+    } else if (e.target.id === 'custom-colors') {
+
+    } else if (e.target.id === 'black-and-white') {
+
+    } else if (e.target.id === 'eraser') {
+        
+    } else if (e.target.id === 'clear-all') {
+
+    }
+})
